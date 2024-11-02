8301fc4af09e925dcd39d7e17676a67a31e9e159
Your Name <you@example.com>
2024-11-02 16:39:46 +0530

    cahnges

---
+++ 04-functions.sh
@@ -0,0 +1,16 @@
+# If we assign a name to a set of commands is called as function
+
+# Declare function
+
+sample() {
+  echo Hello World from Function
+}
+
+# Call the function
+
+sample
+
+# Special Variables
+# $1 - $n
+# $* - All arguments
+# $# - No of arguments
